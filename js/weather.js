10a4dcc39fa947ce928ddaf3253f7b86f718c697
conlanwl <6504347@qq.com>
2022-03-09 17:53:27 +0800

    Site updated: 2022-03-09 17:53:27

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "0000FF",
+    "tmpSize": "16",
+    "cityColor": "FFE699",
+    "citySize": "16",
+    "aqiColor": "4A86E8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "300",
+    "top": "10",
+    "key": "92c61b9850254fbca171b83c37da9b11"
+  }
+}
