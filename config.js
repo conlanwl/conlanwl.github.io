cf90d280a628e814444c5614d965f48ab7f2388e
康兰网络(conlan) <45914862+conlanwl@users.noreply.github.com>
2023-02-24 19:20:32 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-//在支付软件中生成收款码，并使用解码工具对其进行解码，将解码的内容填入
-var pay_set = [{
-    "alipay":"https://qr.alipay.com/stx18076z96b7xfff4bzu79",
-    "wechat":"https://payapp.wechatpay.cn/qrpay/order/home2?key=idc_CHNDVI_9hECYjNZIWOUyJQdGBN4CA--",
-    "qq":"此处填写QQ支付的(如果您使用没有QQ支付的版本，请删除此行)"
-}];
